d2e00f4beefef418562f31336ac64d8f2d138872
Eddie-Muffin <hallperryedmond21@gmail.com>
2023-06-05 17:12:54 +0000

    Calories comment

---
+++ CaloriesF-end/calories.js
@@ -0,0 +1,18 @@
+//Drop down Menu
+    function MenuDropBtn(){
+        console.log("buttonClick")
+        document.getElementById("myMenu").classList.toggle("show");
+    }
+
+    window.onclick = function(event){
+        if(!event.target.matches('.dropbtn')){
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for(i = 0; i < dropdowns.length; i++){
+                var openDropdown = dropdowns[i];
+                if(openDropdown.classList.contains('show')){
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
